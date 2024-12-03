df8fe5f1368ec463a492ad14c637ca818e4e785a
tharindupr <tharindu.prf@gmail.com>
2024-12-03 16:11:45 +0000

    read vars from config

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"veracity_node/internal/routes"
+	"veracity_node/config"
+	"fmt"
+	"os"
+	"log"
 )
 
 func main() {
@@ -10,6 +14,26 @@ func main() {
 	// initializers.ConnectToDB()
 
 	// Set up and start the router
+
+	// Set a custom config path via environment variable or default to "./config"
+	configPath := "./config"
+	if path := os.Getenv("CONFIG_PATH"); path != "" {
+		configPath = path
+	}
+
+	// Load configuration
+	cfg, err := config.LoadConfig(configPath)
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
+	// Use the configuration
+	fmt.Printf("Server running on %s:%d\n", cfg.Server.Host, cfg.Server.Port)
+	fmt.Printf("CA Certificate Path: %s\n", cfg.CACertPath)
+	// Print the content of the CA certificate
+	fmt.Println("CA Certificate Content:")
+	fmt.Println(cfg.CACertContent)
+
 	r := router.SetupRouter()
 	r.Run(":8080")
 }
